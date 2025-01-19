0c2079340e3f3ec0a4e40143c0c8d0a3888e64c2
Kevin Ölmheden <Kevinolm10@gmail.com>
2025-01-19 21:48:30 +0000

    Add sale, sale category and accessories url patterns

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('products/category/<slug:category_slug>/', views.product_category, name='product_category'),
+    path('sale/', views.sale_products, name='sale_products'),
+    path('sale/category/<slug:category_slug>/', views.sale_category, name='sale_category'),
+    path('accessories/', views.all_accessories, name='all_accessories'),
 ]
