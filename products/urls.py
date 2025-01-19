115fcbed46030da91a215cb64163d2a6c45cdc6c
Kevin Ölmheden <Kevinolm10@gmail.com>
2025-01-19 18:38:05 +0000

    Create url path for the categories

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='all_products'),
+    path('products/category/<slug:category_slug>/', views.product_category, name='product_category'),
 ]
