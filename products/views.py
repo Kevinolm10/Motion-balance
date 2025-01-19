adee27fa964ec13c860a622c304b7849e8cc28b9
Kevin Ölmheden <Kevinolm10@gmail.com>
2025-01-19 18:38:37 +0000

    Create the category view

--- products/views.py
+++ products/views.py
@@ -12,3 +12,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+
+def product_category(request, category_slug):
+    try:
+        category = Category.objects.get(slug=category_slug)
+        products = Product.objects.filter(category=category)  # Adjust filtering as necessary
+    except Category.DoesNotExist:
+        products = []  # Or handle the error accordingly
+
+    return render(request, 'products/product_category.html', {
+        'category': category,
+        'products': products,
+    })
