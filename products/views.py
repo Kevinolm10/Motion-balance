94c786dfaa5e834d16c2410c9bacd591b5c94676
Kevin Ölmheden <Kevinolm10@gmail.com>
2025-01-19 21:49:48 +0000

    Remove unecessary view function for products and add a view for all accessories

--- products/views.py
+++ products/views.py
@@ -13,16 +13,21 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-
-
 def product_category(request, category_slug):
-    try:
-        category = Category.objects.get(slug=category_slug)
-        products = Product.objects.filter(category=category)  # Adjust filtering as necessary
-    except Category.DoesNotExist:
-        products = []  # Or handle the error accordingly
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'products/category_products.html', {'category': category, 'products': products})
 
-    return render(request, 'products/product_category.html', {
-        'category': category,
-        'products': products,
-    })
+def sale_products(request):
+    products = Product.objects.filter(discount_price__isnull=False)
+    return render(request, 'products/sale_products.html', {'products': products})
+
+def sale_category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category, discount_price__isnull=False)
+    return render(request, 'products/sale_category_products.html', {'category': category, 'products': products})
+
+def all_accessories(request):
+    # Fetch all products in the "accessories" category
+    accessories = Product.objects.filter(category__slug='accessories')
+    return render(request, 'products/accessories.html', {'accessories': accessories})
