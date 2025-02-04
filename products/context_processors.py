9227cf9451334217adeb4ca0b3efb4b67c21baad
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-04 14:28:39 +0100

    Rework the context processor for the parent categories

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,8 +1,13 @@
 from .models import Category
 
 def categories_context(request):
+    # Fetch categories grouped by their parent category
+    product_categories = Category.objects.filter(parent__nav_element="products")
+    accessory_categories = Category.objects.filter(parent__nav_element="accessories")
+    sale_categories = Category.objects.filter(parent__nav_element="sale")
+
     return {
-        'product_categories': Category.objects.filter(nav_element='products'),
-        'accessory_categories': Category.objects.filter(nav_element='accessories'),
-        'sale_categories': Category.objects.filter(nav_element='sale'),
+        'product_categories': product_categories,
+        'accessory_categories': accessory_categories,
+        'sale_categories': sale_categories
     }
