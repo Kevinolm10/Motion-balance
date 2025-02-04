f89407a8ddb62d6929e46e512975df3bcdc43acc
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-04 14:44:27 +0100

    Clean up the urls and make them work as intended with the rest of the code

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
-    path('products/category/<slug:category_slug>/', views.product_category, name='product_category'),
-    path('sale/', views.sale_products, name='sale_products'),
-    path('sale/category/<slug:category_slug>/', views.sale_category, name='sale_category'),
-    path('accessories/', views.all_accessories, name='all_accessories'),
+    path('category/<slug:category_slug>/', views.product_category, name='product_category'),  # Updated to use one template
+    path('sale/', views.sale_products, name='sale_products'),  # This will filter products that are on sale
+    path('accessories/', views.all_accessories, name='all_accessories'),  # This will filter accessories
     path('subcategory/<slug:subcategory_slug>/', views.product_by_subcategory, name='product_by_subcategory'),
 ]
