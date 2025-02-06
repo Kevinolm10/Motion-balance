6e4d2e4bfab51dbec785a3c2237d9cb0cdbd4248
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-07 00:14:15 +0100

    Change the routing to work with the reworked views and models

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
-    path('category/<slug:category_slug>/', views.product_category, name='product_category'),  # Updated to use one template
-    path('sale/', views.sale_products, name='sale_products'),  # This will filter products that are on sale
-    path('accessories/', views.all_accessories, name='all_accessories'),  # This will filter accessories
+    path('category/<slug:category_slug>/', views.product_category, name='product_category'),
+    path('sale/', views.sale_products, name='sale_products'),
+    path('accessories/', views.all_accessories, name='all_accessories'),
     path('subcategory/<slug:subcategory_slug>/', views.product_by_subcategory, name='product_by_subcategory'),
 ]
