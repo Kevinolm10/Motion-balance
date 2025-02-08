b212530931345893359e49dd28524e97b9754191
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-08 16:22:46 +0100

    Add view for profile, orders, feedback and wishlist

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
+from orders.models import Order
+from products.models import ProductFeedback
+from wishlist.models import WishlistItem
 
-# Create your views here.
+def user_profile(request):
+    try:
+        profile = UserProfile.objects.get(user=request.user)
+    except UserProfile.DoesNotExist:
+        profile = UserProfile(user=request.user)
+        profile.save()
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('user_profile')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = Order.objects.filter(user=request.user)
+    feedbacks = ProductFeedback.objects.filter(user=request.user)
+    wishlist_items = WishlistItem.objects.filter(wishlist__user=request.user)
+
+    return render(request, 'user_profile/user_profile.html', {
+        'form': form,
+        'orders': orders,
+        'feedbacks': feedbacks,
+        'wishlist_items': wishlist_items,
+    })
