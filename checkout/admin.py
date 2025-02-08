675c1903705271f8a002a92edf80772344dcb308
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-08 23:02:55 +0100

    Change price fields for order item

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,7 +3,7 @@ from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    readonly_fields = ('product', 'quantity', 'price')
+    readonly_fields = ('product', 'product_name', 'product_price', 'quantity', 'total_price')
     extra = 0
 
 @admin.register(Order)
@@ -11,7 +11,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'user', 'status', 'created_at', 'grand_total')
     list_filter = ('status', 'created_at', 'updated_at')
     search_fields = ('order_number', 'user__username', 'full_name', 'email')
-    readonly_fields = ('order_number', 'created_at', 'updated_at', 'grand_total', 'transaction_id', 'payment_method')
+    readonly_fields = ('order_number', 'created_at', 'updated_at', 'grand_total')
     fieldsets = (
         (None, {
             'fields': ('order_number', 'user', 'status', 'created_at', 'updated_at')
@@ -20,16 +20,16 @@ class OrderAdmin(admin.ModelAdmin):
             'fields': ('full_name', 'email', 'phone_number')
         }),
         ('Shipping Information', {
-            'fields': ('shipping_address', 'billing_address', 'city', 'country', 'postcode')
+            'fields': ('shipping_address', 'city', 'country', 'postcode')
         }),
         ('Order Details', {
-            'fields': ('subtotal', 'delivery_cost', 'discount_amount', 'grand_total', 'transaction_id', 'payment_method', 'wishlist')
+            'fields': ('subtotal', 'delivery_cost', 'discount_amount', 'grand_total', 'wishlist')
         }),
     )
     inlines = [OrderItemInline]
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'product', 'quantity', 'price')
+    list_display = ('order', 'product', 'product_name', 'product_price', 'quantity', 'total_price')
     search_fields = ('order__order_number', 'product__name')
-    readonly_fields = ('order', 'product', 'quantity', 'price')
+    readonly_fields = ('order', 'product', 'product_name', 'product_price', 'quantity', 'total_price')
