f901b0e2c6e519112bed13f3a0285bfaf06f8710
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-08 14:50:40 +0100

    Add the Wishlist item display from the new model

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Wishlist
+from .models import Wishlist, WishlistItem
 
-# Register your models here.
-
-
+# Wishlist Admin
 @admin.register(Wishlist)
 class WishlistAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'product', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('user__username', 'product__name')
+    list_display = ('user',)
+    search_fields = ('user__username',)
+
+# WishlistItem Admin
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('wishlist', 'product', 'created_at')
+    list_filter = ('created_at', 'wishlist', 'product')
+    search_fields = ('wishlist__user__username', 'product__name')
