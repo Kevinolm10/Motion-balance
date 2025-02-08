3f8811ad4d300e775803c53975d5e8e48bb06e3c
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-08 14:49:53 +0100

    Remove the email field since it is included in the django user model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 # User Profile Model
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     address = models.TextField()
     phone_number = models.CharField(max_length=15)
 
