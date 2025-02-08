6991375e832e0ff9e278cbfb104601264f2ba8a2
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-08 14:49:17 +0100

    Remove email display field

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile
 
-# Register your models here.
-# UserProfile Admin
-@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name', 'last_name', 'email', 'phone_number')
-    search_fields = ('first_name', 'last_name', 'email', 'user__username')
+    list_display = ('user', 'first_name', 'last_name')  # Ensure 'email' exists in UserProfile
+
+admin.site.register(UserProfile, UserProfileAdmin)
