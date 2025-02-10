8313c95a6a2dc0cf657f3160f6e0c0e57b63c4ff
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-10 20:50:48 +0100

    Create the form and product details files

---
+++ products/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Product
+
+class ProductFilterForm(forms.Form):
+    min_price = forms.DecimalField(required=False, decimal_places=2, max_digits=10, label='Min Price')
+    max_price = forms.DecimalField(required=False, decimal_places=2, max_digits=10, label='Max Price')
+    min_discount = forms.DecimalField(required=False, decimal_places=2, max_digits=5, label='Min Discount')
+    max_discount = forms.DecimalField(required=False, decimal_places=2, max_digits=5, label='Max Discount')
+    min_rating = forms.DecimalField(required=False, decimal_places=1, max_digits=2, label='Min Rating')
+    max_rating = forms.DecimalField(required=False, decimal_places=1, max_digits=2, label='Max Rating')
