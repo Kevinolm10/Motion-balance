25ced9ad2140c8ea10a0fa80aa08feb346f373f1
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-10 21:22:29 +0100

    Create url routing for the product details page

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,11 @@ from . import views
 # URL patterns for products app
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('product/<int:product_id>/', views.product_details, name='product_details'),
     path('category/<slug:category_slug>/', views.product_category, name='product_category'),
     path('sale/', views.sale_products, name='sale_products'),
     path('accessories/', views.all_accessories, name='all_accessories'),
     path('subcategory/<slug:subcategory_slug>/', views.product_by_subcategory, name='product_by_subcategory'),
-     path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove_from_wishlist/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
