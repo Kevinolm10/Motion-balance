6dd1b852e07bfee709349403639e55145487412d
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-11 22:45:00 +0100

    Create routing for the checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('orders/', views.order_list, name='order_list'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('checkout/', views.checkout, name='checkout'),  # Add this line
 ]
