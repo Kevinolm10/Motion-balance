f0865be04d2e5e92a3ea55d96beff31d2e7a4779
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-12 23:33:57 +0100

    Add accessability and good seo practice to the html templates

--- user_profile/views.py
+++ user_profile/views.py
@@ -6,13 +6,17 @@ from checkout.models import Order
 from products.models import ProductFeedback
 from wishlist.models import WishlistItem
 
+# Create your views here.
+# User profile view
 def user_profile(request):
+    # Get the user profile or create a new one if it doesn't exist
     try:
         profile = UserProfile.objects.get(user=request.user)
     except UserProfile.DoesNotExist:
         profile = UserProfile(user=request.user)
         profile.save()
 
+# Check if the form has been submitted
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
@@ -22,10 +26,12 @@ def user_profile(request):
     else:
         form = UserProfileForm(instance=profile)
 
+# Get the user's orders, feedbacks and wishlist items
     orders = Order.objects.filter(user=request.user)
     feedbacks = ProductFeedback.objects.filter(user=request.user)
     wishlist_items = WishlistItem.objects.filter(wishlist__user=request.user)
 
+# Render the user profile template
     return render(request, 'user_profile/user_profile.html', {
         'form': form,
         'orders': orders,
