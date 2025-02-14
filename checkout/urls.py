51fd606c3721dcbe35887a0e75eba616a16d4d33
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-14 17:03:48 +0100

    Debug and change the urls for the checkout app

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 # URL patterns for checkout app
 urlpatterns = [
-    path('orders/', views.order_list, name='order_list'),
-    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('checkout/', views.checkout, name='checkout'),  # Add this line
+    path('', views.checkout, name='checkout'),
+    
 ]
