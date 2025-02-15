b2e048cc87bd8166ffecfade4f9fb038093633c8
kevin_Olmheden <Kevinolm10@gmail.com>
2025-02-15 14:42:56 +0100

    Add the order details url

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
 ]
 
